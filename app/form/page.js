afa3a87d8a935dea236b2c5e7d5413da73dc11c9
Dina <dinagiacobbi@gmail.com>
2024-07-10 13:53:12 -0800

    Adding folder structures and starting form template

---
+++ app/form/page.js
@@ -0,0 +1,19 @@
+
+
+
+function Form() {
+
+
+
+  return (
+    <>
+    <form>
+      <input type="text" name="name" placeholder="Company Name"/>
+      <button type="submit">Submit</button>
+    </form>
+    
+    </>
+  )
+}
+
+export default Form
